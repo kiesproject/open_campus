ec2e2c3357489255bfd3163b60a47b1eb09f4ab7
Asck <asck.bbc@gmail.com>
2019-01-30 14:29:05 +0900

    コメントの編集

--- main_style.js
+++ main_style.js
@@ -1,24 +1,6 @@
 window.onload = function () {
-    console.log("obj.onload");
     try {
-        /*
-         var blossoms = $('.blossom').;
-         blossoms[0].style.top = '10px';
-         blossoms[0].style.left = '100px';
-
-         blossoms[1].style.top   = '50px';
-         blossoms[1].style.left  = '10px';
-
-         blossoms[2].style.top   = '40px';
-         blossoms[2].style.right = '50px';
-
-         blossoms[3].style.top   = '100px';
-         blossoms[3].style.right = '200px';
-         */
         setInterval(dropBlossom, 1000);
-        dropBlossom();
-
-
     }
     catch (e) {
         console.log(e);
