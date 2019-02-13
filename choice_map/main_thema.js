90fa8b8f29b49c9054b6aa9a6202fa53ef23c24e
大谷直也 <tani.naoya1122@gmail.com>
2019-02-13 23:35:13 +0900

    マップのプログラムをindex.htmlに移動,インデントの修正

--- choice_map/main_thema.js
+++ choice_map/main_thema.js
@@ -1,20 +1,83 @@
-var  map;
-var marke;
-var center
-var center = {
-    lat: 36.530442,
-    lat: 136.629382
-};
+var my_size = 17;//拡大サイズ
+var my_latlng; //現在位置用の変数
 function initMap(){
-    map = new google.maps.Map(document.getElementById('map'),{
-        center:center,
-        zoom:19
-    });
-    marker = new google.maps.Marker({
-        position:center,
-        map:map
-    });
-}
+    //geolocaion APIに対応している場合
+    if(navigator.geolocation){
+            //位置情報が取得できる場合
+            navigator.geolocation.getCurrentPosition(
+                //true
+                function(position){
+                    //現在地の緯度経度
+                    my_latlng = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+                    var mapOptions = {
+                        zoom:my_size,
+                        center:my_latlng
+                    };
+                    //mapに地図を埋め込む
+                    var map = new google.maps.Map(document.getElementById("map"),mapOptions);
+                                //自分の位置のマーカー
+                    var my_marker = new google.maps.Marker({
+                        map:map,
+                        position:my_latlng,
+                        icon:"http://maps.google.com/mapfiles/ms/micons/man.png"
+                    });
+                },
+                //false
+                function(error){
+                    switch(error.code){
+                        case 1:// PERMISSION_DENIED
+                            alert("位置情報の利用が許可されていません");
+                            break;
+                        case 2: // POSITION_UNAVAILABLE
+                            alert("現在位置が取得できませんでした");
+                            break;
+                        case 3: // TIMEOUT
+                            alert("タイムアウトになりました");
+                            break;
+                        default:
+                            alert("その他のエラー(エラーコード:"+error.code+")");
+                        break;
+                    }
+                }
+            );    
+        }
+        //APIが対応していない場合
+        else{
+            alert("この端末では位置情報が取得できません");   
+        }
+    }
+    //selectが変更された場合の処理
+    function selectchanged(){
+        //号館の座標を登録しておくリスト
+        var latlng = new Array(4);
+        latlng[0] = new google.maps.LatLng(36.531498, 136.627418);//LC（デバッグ用）
+        latlng[1] = new google.maps.LatLng(36.531802, 136.628244);//7号館
+        latlng[2] = new google.maps.LatLng(36.530442, 136.629382);//8号館
+        latlng[3] = new google.maps.LatLng(36.531240, 136.629425);//23号館
+        //デバッグ用
+        console.log("selectchanged");
+        //selの値を取得
+        var sl = document.getElementById("sel");
+        //selの選択された番号を取得
+        var value = document.myselect.sel.selectedIndex;
+        //mapに書き込み
+        var map = new google.maps.Map(document.getElementById("map"),{
+            zoom:my_size,
+            center:latlng[value]
+
+        });
+        //マーカーの位置の設定
+        var marker = new google.maps.Marker({
+            position:latlng[value],
+            map:map
+        });
+
+        var marker = new google.maps.Marker({
+            position:my_latlng,
+            map:map,
+            icon:"http://maps.google.com/mapfiles/ms/micons/man.png"
+        });
+    }
 
 
 $('#area').change(function(){
