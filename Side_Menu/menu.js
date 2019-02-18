23517b7b653ff92f4c426d2a341aaed437c711f8
Fujii <b1606151@planet.kanazawa-it.ac.jp>
2019-02-18 23:34:27 +0900

    スクロール処理、命名規則、idクラス分け修正しました。

--- Side_Menu/menu.js
+++ Side_Menu/menu.js
@@ -1,27 +1,21 @@
 $(function(){
     var menu_list = $(".menu_list > li");
     var body = $('body');
-    $('#js_sideMenuBtn').on('click', function(){
-        body.toggleClass('side-open');
-        $(".side_menu").slideToggle();
-        $('#js_overlay, .side_time').on('click', function(){
-            body.removeClass('side-open');
-            $('.side_menu').hide();
+    $("#side_menu_btn").on('click', function(){
+        body.toggleClass('side_open');
+        $("#side_menu").slideToggle();
+        $("#overlay").on('click', function(){
+            body.removeClass('side_open');
+            $("#side_menu").hide();
         });
 
     });
     //menu_listがクリックされた時の処理
     menu_list.click(function(){
-        var menu = $(this).attr("href");
-        var scroll_point = $(menu).offset();
-        
-        body.animate({
-            scrollTop: scroll_point
-        }, 300);
+        var scroll_point = $(this).innerHeight();
     });
-    
-    $('#menu_time, #menu_room, #menu_map').on('click', function(){
-        body.removeClass('side-open');
-        $('.side_menu').hide();
+    $("#menu_time, #menu_room, #menu_map").on('click', function(){
+        body.removeClass('side_open');
+        $("#side_menu").hide();
     });
 });
