4da7a280b05c60f9259bd6c6c3ca9548ed800fac
Fujii <b1606151@planet.kanazawa-it.ac.jp>
2019-02-22 22:54:33 +0900

    プルダウンメニューの項目の追加

---
+++ side_menu/menu.js
@@ -0,0 +1,21 @@
+$(function(){
+    var menu_list = $("#menu_list > li");
+    var body = $('body');
+    $("#side_menu_btn").on('click', function(){
+        body.toggleClass('side_open');
+        $("#side_menu").slideToggle();
+        $("#overlay").on('click', function(){
+            body.removeClass('side_open');
+            $("#side_menu").hide();
+        });
+
+    });
+    //menu_listがクリックされた時の処理
+    menu_list.click(function(){
+        var scroll_point = $(this).innerHeight();
+    });
+    $(".js_menu").on('click', function(){
+        body.removeClass('side_open');
+        $("#side_menu").hide();
+    });
+});
