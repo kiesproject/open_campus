e431db610b1667586516c31a60483e7a248310c3
Asck <asck.bbc@gmail.com>
2019-02-25 03:14:59 +0900

    pull_down.jsで多用されていたパス直書きの整理

--- pull_down.js
+++ pull_down.js
@@ -1,8 +1,11 @@
 $(function(){
-    $('.timetable tr p').hide();     //ロード初期の詳細は非表示
+    pulldown_tr = '#times .section_info table tbody tr'
+    $(pulldown_tr + ' p').hide();     //ロード初期の詳細は非表示
+
+
     
     //アイコン横の<p>要素のクリックで詳細を表示・非表示の切り替え
-    $('.timetable tr span').click(function(){
+    $(pulldown_tr + ' span').click(function(){
         if($(this).prev().attr('id') == 'selected'){
             $(this).prev('.icon').attr('id','').animate( { opacity: 1 }, {
                 duration: 1000,
@@ -23,7 +26,7 @@ $(function(){
     });
 
     //アイコンクリックでも、詳細を表示・非表示の切り替え
-    $('.timetable .icon').click(function(){
+    $(pulldown_tr +  ' .icon').click(function(){
         if($(this).attr('id') == 'selected'){
             $(this).attr('id','').animate( { opacity: 1 }, {
                 duration: 1000,
@@ -43,3 +46,8 @@ $(function(){
         }
     });
 });
+
+function animation(clicked_dom) {
+
+
+}
