056353fabf3d918e7587af7f006d855b7fe4b923
Asck <asck.bbc@gmail.com>
2019-02-25 03:35:45 +0900

    pulldown.jsに記載された内容の多い無名関数の関数化

--- pull_down.js
+++ pull_down.js
@@ -1,53 +1,51 @@
-$(function(){
+$(function () {
     pulldown_tr = '#times .section_info table tbody tr'
     $(pulldown_tr + ' p').hide();     //ロード初期の詳細は非表示
 
-
-    
     //アイコン横の<p>要素のクリックで詳細を表示・非表示の切り替え
-    $(pulldown_tr + ' span').click(function(){
-        if($(this).prev().attr('id') == 'selected'){
-            $(this).prev('.icon').attr('id','').animate( { opacity: 1 }, {
-                duration: 1000,
-                step: function (now) {
-                    $(this).css({ transform: 'rotate(' + now * 0 + 'deg)' })
-                }
-            });
-            $(this).next('p').hide(600);
-        }else{
-            $(this).prev('.icon').attr('id','selected').animate( { opacity: 1 }, {
-                duration: 1000,
-                step: function (now) {
-                    $(this).css({ transform: 'rotate(' + now * 90 + 'deg)' })
-                }
-            });
-            $(this).next('p').show(600);
-        }
-    });
+    $(pulldown_tr + ' span').click(pulldown_content_by_header);
 
     //アイコンクリックでも、詳細を表示・非表示の切り替え
-    $(pulldown_tr +  ' .icon').click(function(){
-        if($(this).attr('id') == 'selected'){
-            $(this).attr('id','').animate( { opacity: 1 }, {
-                duration: 1000,
-                step: function (now) {
-                    $(this).css({ transform: 'rotate(' + now * 0 + 'deg)' })
-                }
-            });
-            $(this).next().next('p').hide(600);
-        }else{
-            $(this).attr('id','selected').animate( { opacity: 1 }, {
-                duration: 1000,
-                step: function (now) {
-                    $(this).css({ transform: 'rotate(' + now * 90 + 'deg)' })
-                }
-            });
-            $(this).next().next('p').show(600);
-        }
-    });
+    $(pulldown_tr + ' .icon').click(pulldown_content_by_icon);
 });
 
-function animation(clicked_dom) {
+function pulldown_content_by_header() {
+    $
+    if ($(this).prev().attr('id') == 'selected') {
+        $(this).prev('.icon').attr('id', '').animate({opacity: 1}, {
+            duration: 1000,
+            step: function (now) {
+                $(this).css({transform: 'rotate(' + now * 0 + 'deg)'})
+            }
+        });
+        $(this).next('p').hide(600);
+    } else {
+        $(this).prev('.icon').attr('id', 'selected').animate({opacity: 1}, {
+            duration: 1000,
+            step: function (now) {
+                $(this).css({transform: 'rotate(' + now * 90 + 'deg)'})
+            }
+        });
+        $(this).next('p').show(600);
+    }
+}
 
-
+function pulldown_content_by_icon() {
+    if ($(this).attr('id') == 'selected') {
+        $(this).attr('id', '').animate({opacity: 1}, {
+            duration: 1000,
+            step: function (now) {
+                $(this).css({transform: 'rotate(' + now * 0 + 'deg)'})
+            }
+        });
+        $(this).next().next('p').hide(600);
+    } else {
+        $(this).attr('id', 'selected').animate({opacity: 1}, {
+            duration: 1000,
+            step: function (now) {
+                $(this).css({transform: 'rotate(' + now * 90 + 'deg)'})
+            }
+        });
+        $(this).next().next('p').show(600);
+    }
 }
