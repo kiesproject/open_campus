5f4cbb8215e184ff5b00f1d52c68b68acd914452
宮澤　瑛至 <b1800722@planet.kanazawa-it.ac.jp>
2019-02-27 21:26:57 +0900

    今のところの修正をプッシュします。

--- pull_down.js
+++ pull_down.js
@@ -1,54 +1,41 @@
 $(function(){
-    $('.timetable tr p').hide();     //ロード初期の詳細は非表示
+    pulldouwn_tr = '#times .section_info table tbody tr'
+    pulldouwn_img = '#class_room .section_info table tbody tr'
 
-    //マウスカーソルが重なると背景色をグレーに
-    $('.timetable tr span')
-        .mouseover(function(){
-            $(this).css('background','grey');
-        })
-        .mouseout(function(){
-            $(this).css('background','white');
-        });
-    
-    //アイコン横の<p>要素のクリックで詳細を表示・非表示の切り替え
-    $('.timetable tr span').click(function(){
-        if($(this).prev().attr('id') == 'selected'){
-            $(this).prev('.icon').attr('id','').animate( { opacity: 1 }, {
-                duration: 1000,
-                step: function (now) {
-                    $(this).css({ transform: 'rotate(' + now * 0 + 'deg)' })
-                }
-            });
-            $(this).next('p').hide(600);
-        }else{
-            $(this).prev('.icon').attr('id','selected').animate( { opacity: 1 }, {
-                duration: 1000,
-                step: function (now) {
-                    $(this).css({ transform: 'rotate(' + now * 90 + 'deg)' })
-                }
-            });
-            $(this).next('p').show(600);
-        }
-    });
+    $(pulldouwn_tr + ' p').hide();     //ロード初期の詳細は非表示
+    //$(pulldouwn_img + ' img').hide();     //ロード初期の詳細は非表示
 
-    //アイコンクリックでも、詳細を表示・非表示の切り替え
-    $('.timetable .icon').click(function(){
-        if($(this).attr('id') == 'selected'){
-            $(this).attr('id','').animate( { opacity: 1 }, {
-                duration: 1000,
-                step: function (now) {
-                    $(this).css({ transform: 'rotate(' + now * 0 + 'deg)' })
-                }
-            });
-            $(this).next().next('p').hide(600);
-        }else{
-            $(this).attr('id','selected').animate( { opacity: 1 }, {
-                duration: 1000,
-                step: function (now) {
-                    $(this).css({ transform: 'rotate(' + now * 90 + 'deg)' })
-                }
-            });
-            $(this).next().next('p').show(600);
-        }
-    });
+    $(pulldouwn_tr).click(pulldouwn_content);
 });
+
+function pulldouwn_content(){
+    if($(this).find('.icon').attr('id') == 'selected'){
+        $(this).find('.icon').attr('id','').animate(
+            {
+                opacity: 1
+            },
+            {
+                duration: 1000,
+                step: function (now){
+                    $(this).css({transform: 'rotate(0deg)'});
+                }
+            }
+        );
+        $(this).find('p').hide(600);
+        // $(this).find('img').hide(600);
+    }else{
+        $(this).find('.icon').attr('id','selected').animate(
+            {
+                opacity: 1
+            },
+            {
+                duration: 1000,
+                step: function (now){
+                    $(this).css({transform: 'rotate(90deg)'});
+                }
+            }
+        );
+        $(this).find('p').show(600);
+        // $(this).find('img').hide(600);
+    }
+}
