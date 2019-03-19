5484fafa281be32c235d90d5f2ad469811d84aad
宮澤　瑛至 <b1800722@planet.kanazawa-it.ac.jp>
2019-03-19 18:47:59 +0900

    show_content関数に引数を追加して、コードを簡略化

--- minimap_jump.js
+++ minimap_jump.js
@@ -1,36 +1,9 @@
 $(function(){
     var minimap_link = '.section_info table a:contains("教室：")';
-    $(minimap_link).click(link_pulldown);
+    $(minimap_link).click(link_show);
 });
 
-function link_pulldown(){   //プルダウン処理
-    var content = $(this).attr('href');
-    
-    if($(content).find('.icon').attr('id') == 'selected'){
-        $(content).find('.icon').attr('id','').animate(
-            {
-                opacity: 1
-            },
-            {
-                duration: 1000,
-                step: function (now){
-                    $(this).css({transform: 'rotate(0deg)'});
-                }
-            }
-        );
-        $(content).find('.pulldown_content').hide(600);
-    }else{
-        $(content).find('.icon').attr('id','selected').animate(
-            {
-                opacity: 1
-            },
-            {
-                duration: 1000,
-                step: function (now){
-                    $(this).css({transform: 'rotate(90deg)'});
-                }
-            }
-        );
-        $(content).find('.pulldown_content').show(600);
-    }
+function link_show(){
+    var obj = $(this).attr('href');
+    show_content(obj);
 }
