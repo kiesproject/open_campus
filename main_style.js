70e2e3b367b3f888c63487be73cec0089b3a8162
Asck <asck.bbc@gmail.com>
2019-03-24 15:56:03 +0900

    fix_blossom_styleの統合作業

--- main_style.js
+++ main_style.js
@@ -1,33 +1,38 @@
 window.onload = function () {
     try {
-        setInterval(dropBlossom, 1000);
+        setInterval(dropBlossom, 700);
     }
     catch (e) {
         console.log(e);
     }
 }
 function dropBlossom() {
-    var browsWidth = window.innerWidth;
-    var browsHeight = window.innerHeight;
-    var blosFirstLeft = Math.random() * browsWidth - 100;
-    var blosFallDist = Math.random() * browsHeight + 400;
-    $('body')
+    var fallen_right_dist = document.body.scrollWidth;
+    var fallen_down_dist = fallen_right_dist * 3;
+    var fallen_duration = (fallen_down_dist / 150) * 1000; //落ちる時間＝150px毎に1000[ms]
+    //-fallen_down_dist[px]～document.body.scrollWidht[px]までのランダムな値
+    var first_top_offset = Math.random()*(fallen_down_dist + document.body.scrollHeight) - fallen_down_dist;
+
+
+    $('#blossom_area')
         .prepend("<img class='blossom' " +
             "src='image/blossom100.png' " +
-            "style='left:" + blosFirstLeft + "px;" +
-            "top:-100px;'/>");
+            "style='" +
+            "left:-100px;" +
+            "top :" + first_top_offset + "px;" +
+            "'/>");
     $('.blossom:first')
         .animate(
             {
-                left: blosFirstLeft + blosFallDist / 3 + 'px',
-                top: blosFallDist + 'px',
-                opacity: '0'
+                left: fallen_right_dist + 'px',
+                top: (fallen_down_dist + first_top_offset) + 'px',
             },
             {
-                duration: 10000,
+                duration: fallen_duration,
+                easing: "linear",
                 step: function (now) {
                     $(this)
-                        .css({transform: 'rotate(' + (now * 360) + 'deg)'});
+                        .css({transform: 'rotate(' + now + 'deg)'});
                 },
                 complete: function () {
                     this.remove();
