2696766ce773262feff67246a41ec143bb0f7008
宮澤　瑛至 <b1800722@planet.kanazawa-it.ac.jp>
2019-04-04 22:25:27 +0900

    プルダウン機能の追加

--- overall_schedule.js
+++ overall_schedule.js
@@ -2,11 +2,12 @@ $(function(){
     var overall_title = '#overall .section_title';
     var overall = '.overall_schedule .pulldown_content'
     $(overall_title).click(overall_pulldown);
-    // $(overall).hide();
+    $(overall).hide();
 });
 
 function overall_pulldown(){
-    var obj_path = '.overall_schdule';
-    var obj = $(obj_path);
+    //$(this).css('color','red');
+    var overall_path = '.overall_schedule';
+    var obj = $(overall_path);
     show_content(obj);
 }
