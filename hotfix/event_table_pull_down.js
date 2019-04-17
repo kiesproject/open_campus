c47634e47f3b1c6701d66fb4ab52d5951ef27dda
大谷直也 <tani.naoya1122@gmail.com>
2019-04-17 15:11:11 +0900

    pull_downで一応隠せるように実装(バグあり)

--- hotfix/event_table_pull_down.js
+++ hotfix/event_table_pull_down.js
@@ -1,40 +1,12 @@
 $(function(){
-    pulldown_tr = '.section_info table tbody tr';
-    $(pulldown_tr + ' .pulldown_content').hide();
-    $(pulldown_tr).click(pulldown);
+    var overall_title = '#event_table';
+    var overall = overall_title + ' .pulldown_content';
+    
+    $(overall).hide();
+    $(overall_title).click(overall_pulldown);
 });
 
-function pulldown(){
+function overall_pulldown(){
     var obj = $(this);
     show_content(obj);
-}
-
-function show_content(content){
-    if(content.find('.icon').attr('id') == 'selected'){
-        content.find('.icon').attr('id','').animate(
-            {
-                opacity: 1
-            },
-            {
-                duration: 1000,
-                step: function (now){
-                    $(this).css({transform: 'rotate(0deg)'});
-                }
-            }
-        );
-        content.find('.pulldown_content').hide(600);
-    }else{
-        content.find('.icon').attr('id','selected').animate(
-            {
-                opacity: 1
-            },
-            {
-                duration: 1000,
-                step: function (now){
-                    $(this).css({transform: 'rotate(90deg)'});
-                }
-            }
-        );
-        content.find('.pulldown_content').show(600);
-    }
 }
